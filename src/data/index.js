1c95d3567000f46a698e716047a409999cb900a2
juliotwo <juliotwo1996@gmail.com>
2025-09-02 20:46:12 -0600

    fix

--- src/data/index.js
+++ src/data/index.js
@@ -1,225 +1,225 @@
-export const pageName = "Derkleens";
-export const email = "info@derkleens.com";
+export const pageName = 'Derkleens';
+export const email = 'info@derkleens.com';
 export const address =
-  "AV TINTORETO 92 1 ER OFICINA 101 COLONIA CIUDAD DE LOS DEPORTES BENITO JUAREZ CIUDAD DE MEXICO CP 03710";
-export const phoneNumber = "+525520725849";
-export const phoneWhatsApp = "+525520725849";
+  'AV TINTORETO 92 1 ER OFICINA 101 COLONIA CIUDAD DE LOS DEPORTES BENITO JUAREZ CIUDAD DE MEXICO CP 03710';
+export const phoneNumber = '+525520725849';
+export const phoneWhatsApp = '+525520725849';
 export const navbarOptions = [
   {
-    href: "/",
-    name: "Home",
+    href: '/',
+    name: 'Home',
   },
   {
-    href: "/#about",
-    name: "About Us",
+    href: '/#about',
+    name: 'About Us',
   },
   {
-    href: "/#courses",
-    name: "Products",
+    href: '/#courses',
+    name: 'Products',
   },
   {
-    href: "/#review",
-    name: "Review",
+    href: '/#review',
+    name: 'Review',
   },
   {
-    href: "/contact",
-    name: "Contact",
+    href: '/contact',
+    name: 'Contact',
   },
 ];
 
 export const productsData = [
   {
     id: 995,
-    name: "Leadership Case Study Library",
+    name: 'Leadership Case Study Library',
     category: null,
     description:
-      "Access to real-world case studies showcasing successful leadership strategies and lessons learned.",
-    price: "23.00",
+      'Access to real-world case studies showcasing successful leadership strategies and lessons learned.',
+    price: '23.00',
     stock: 100,
-    content: "",
-    product_type: "NORMAL",
+    content: '',
+    product_type: 'NORMAL',
     image:
-      "https://cms-webserver-statics.s3.amazonaws.com/media/products/cu_12.jpg",
+      'https://cms-webserver-statics.s3.amazonaws.com/media/products/cu_12.jpg',
     image2: null,
     image3: null,
   },
   {
     id: 996,
-    name: "Certification & Progress Tracking Dashboard",
+    name: 'Certification & Progress Tracking Dashboard',
     category: null,
     description:
-      "Digital platform to monitor course completion, achievements, and earned certifications.",
-    price: "16.00",
+      'Digital platform to monitor course completion, achievements, and earned certifications.',
+    price: '16.00',
     stock: 100,
-    content: "",
-    product_type: "NORMAL",
+    content: '',
+    product_type: 'NORMAL',
     image:
-      "https://cms-webserver-statics.s3.amazonaws.com/media/products/cu_13.jpg",
+      'https://cms-webserver-statics.s3.amazonaws.com/media/products/cu_13.jpg',
     image2: null,
     image3: null,
   },
   {
     id: 990,
-    name: "Data Analytics Masterclass",
+    name: 'Data Analytics Masterclass',
     category: null,
     description:
-      "Unlock the power of data with our comprehensive masterclass on data analytics. Learn how to collect, analyze, and interpret data to make informed business decisions and gain a competitive edge in the global market.",
-    price: "25.00",
+      'Unlock the power of data with our comprehensive masterclass on data analytics. Learn how to collect, analyze, and interpret data to make informed business decisions and gain a competitive edge in the global market.',
+    price: '25.00',
     stock: 100,
-    content: "",
-    product_type: "NORMAL",
+    content: '',
+    product_type: 'NORMAL',
     image:
-      "https://cms-webserver-statics.s3.amazonaws.com/media/products/course-2.webp",
+      'https://cms-webserver-statics.s3.amazonaws.com/media/products/course-2.webp',
     image2: null,
     image3: null,
   },
   {
     id: 991,
-    name: "Supply Chain Optimization Workshop",
+    name: 'Supply Chain Optimization Workshop',
     category: null,
     description:
-      "Enhance your supply chain management skills with our interactive workshop. Explore strategies for optimizing supply chain efficiency, improving logistics, minimizing costs, and mitigating risks in a global supply chain context.",
-    price: "250.00",
+      'Enhance your supply chain management skills with our interactive workshop. Explore strategies for optimizing supply chain efficiency, improving logistics, minimizing costs, and mitigating risks in a global supply chain context.',
+    price: '250.00',
     stock: 100,
-    content: "",
-    product_type: "NORMAL",
+    content: '',
+    product_type: 'NORMAL',
     image:
-      "https://cms-webserver-statics.s3.amazonaws.com/media/products/course-8.webp",
+      'https://cms-webserver-statics.s3.amazonaws.com/media/products/course-8.webp',
     image2: null,
     image3: null,
   },
   {
     id: 992,
-    name: "Personalized Leadership Development Plan",
+    name: 'Personalized Leadership Development Plan',
     category: null,
     description:
-      "Tailored roadmap to enhance individual leadership skills and career growth.",
-    price: "21.00",
+      'Tailored roadmap to enhance individual leadership skills and career growth.',
+    price: '21.00',
     stock: 100,
-    content: "",
-    product_type: "NORMAL",
+    content: '',
+    product_type: 'NORMAL',
     image:
-      "https://cms-webserver-statics.s3.amazonaws.com/media/products/cou_9_.jpg",
+      'https://cms-webserver-statics.s3.amazonaws.com/media/products/cou_9_.jpg',
     image2: null,
     image3: null,
   },
   {
     id: 993,
-    name: "Team Dynamics Assessment Toolkit",
+    name: 'Team Dynamics Assessment Toolkit',
     category: null,
     description:
-      "Tools and surveys to evaluate and improve team collaboration and communication.",
-    price: "15.00",
+      'Tools and surveys to evaluate and improve team collaboration and communication.',
+    price: '15.00',
     stock: 100,
-    content: "",
-    product_type: "NORMAL",
+    content: '',
+    product_type: 'NORMAL',
     image:
-      "https://cms-webserver-statics.s3.amazonaws.com/media/products/cu_10_.jpg",
+      'https://cms-webserver-statics.s3.amazonaws.com/media/products/cu_10_.jpg',
     image2: null,
     image3: null,
   },
   {
     id: 994,
-    name: "Executive Coaching Sessions",
+    name: 'Executive Coaching Sessions',
     category: null,
     description:
-      "One-on-one coaching with experienced mentors to strengthen leadership effectiveness.",
-    price: "18.00",
+      'One-on-one coaching with experienced mentors to strengthen leadership effectiveness.',
+    price: '18.00',
     stock: 100,
-    content: "",
-    product_type: "NORMAL",
+    content: '',
+    product_type: 'NORMAL',
     image:
-      "https://cms-webserver-statics.s3.amazonaws.com/media/products/cu_11.jpg",
+      'https://cms-webserver-statics.s3.amazonaws.com/media/products/cu_11.jpg',
     image2: null,
     image3: null,
   },
   {
     id: 984,
-    name: "Business Leadership Course",
+    name: 'Business Leadership Course',
     category: null,
     description:
-      "Equip yourself with the essential skills and knowledge to become an effective business leader in the global marketplace. This comprehensive course covers leadership strategies, decision-making, team management, and effective communication techniques.",
-    price: "20.00",
+      'Equip yourself with the essential skills and knowledge to become an effective business leader in the global marketplace. This comprehensive course covers leadership strategies, decision-making, team management, and effective communication techniques.',
+    price: '20.00',
     stock: 100,
-    content: "",
-    product_type: "NORMAL",
+    content: '',
+    product_type: 'NORMAL',
     image:
-      "https://cms-webserver-statics.s3.amazonaws.com/media/products/course-1.webp",
+      'https://cms-webserver-statics.s3.amazonaws.com/media/products/course-1.webp',
     image2: null,
     image3: null,
   },
   {
     id: 985,
-    name: "Technological Innovation Workshop",
+    name: 'Technological Innovation Workshop',
     category: null,
     description:
-      "Stay ahead of the curve with our hands-on workshop focused on technological innovation. Explore emerging technologies, such as artificial intelligence, blockchain, and Internet of Things (IoT), and learn how to leverage them to drive innovation and gain a competitive edge.",
-    price: "129.00",
+      'Stay ahead of the curve with our hands-on workshop focused on technological innovation. Explore emerging technologies, such as artificial intelligence, blockchain, and Internet of Things (IoT), and learn how to leverage them to drive innovation and gain a competitive edge.',
+    price: '129.00',
     stock: 100,
-    content: "",
-    product_type: "NORMAL",
+    content: '',
+    product_type: 'NORMAL',
     image:
-      "https://cms-webserver-statics.s3.amazonaws.com/media/products/course-7.webp",
+      'https://cms-webserver-statics.s3.amazonaws.com/media/products/course-7.webp',
     image2: null,
     image3: null,
   },
   {
     id: 986,
-    name: "Financial Process Efficiency Seminar",
+    name: 'Financial Process Efficiency Seminar',
     category: null,
     description:
       "Optimize your financial processes and enhance operational efficiency with our in-depth seminar. Learn best practices in financial management, budgeting, risk assessment, and cost optimization to streamline your organization's financial operations.",
-    price: "199.00",
+    price: '199.00',
     stock: 100,
-    content: "",
-    product_type: "NORMAL",
+    content: '',
+    product_type: 'NORMAL',
     image:
-      "https://cms-webserver-statics.s3.amazonaws.com/media/products/course-3.webp",
+      'https://cms-webserver-statics.s3.amazonaws.com/media/products/course-3.webp',
     image2: null,
     image3: null,
   },
   {
     id: 987,
-    name: "Strategic Marketing Conference",
+    name: 'Strategic Marketing Conference',
     category: null,
     description:
-      "Discover the latest trends and strategies in strategic marketing at our comprehensive conference. Learn how to develop effective marketing plans, leverage digital channels, and create compelling brand experiences to drive business growth.",
-    price: "42.00",
+      'Discover the latest trends and strategies in strategic marketing at our comprehensive conference. Learn how to develop effective marketing plans, leverage digital channels, and create compelling brand experiences to drive business growth.',
+    price: '42.00',
     stock: 100,
-    content: "",
-    product_type: "NORMAL",
+    content: '',
+    product_type: 'NORMAL',
     image:
-      "https://cms-webserver-statics.s3.amazonaws.com/media/products/course-4.webp",
+      'https://cms-webserver-statics.s3.amazonaws.com/media/products/course-4.webp',
     image2: null,
     image3: null,
   },
   {
     id: 988,
-    name: "International Business Ethics Workshop",
+    name: 'International Business Ethics Workshop',
     category: null,
     description:
-      "Explore the ethical challenges and responsibilities faced by businesses operating in a global context. This interactive workshop delves into topics such as corporate social responsibility, ethical decision-making, and sustainable business practices.",
-    price: "49.00",
+      'Explore the ethical challenges and responsibilities faced by businesses operating in a global context. This interactive workshop delves into topics such as corporate social responsibility, ethical decision-making, and sustainable business practices.',
+    price: '49.00',
     stock: 100,
-    content: "",
-    product_type: "NORMAL",
+    content: '',
+    product_type: 'NORMAL',
     image:
-      "https://cms-webserver-statics.s3.amazonaws.com/media/products/course-5.webp",
+      'https://cms-webserver-statics.s3.amazonaws.com/media/products/course-5.webp',
     image2: null,
     image3: null,
   },
   {
     id: 989,
-    name: "Cross Cultural Communication Seminar",
+    name: 'Cross Cultural Communication Seminar',
     category: null,
     description:
-      "Develop the skills to effectively communicate and collaborate with individuals from diverse cultural backgrounds. This seminar covers cultural intelligence, intercultural communication strategies, and fostering inclusive work environments.",
-    price: "100.00",
+      'Develop the skills to effectively communicate and collaborate with individuals from diverse cultural backgrounds. This seminar covers cultural intelligence, intercultural communication strategies, and fostering inclusive work environments.',
+    price: '100.00',
     stock: 100,
-    content: "",
-    product_type: "NORMAL",
+    content: '',
+    product_type: 'NORMAL',
     image:
-      "https://cms-webserver-statics.s3.amazonaws.com/media/products/course-6.webp",
+      'https://cms-webserver-statics.s3.amazonaws.com/media/products/course-6.webp',
     image2: null,
     image3: null,
   },
@@ -227,11 +227,11 @@ export const productsData = [
 
 export const footerOptions = [
   {
-    href: "/pdf/AP-SDERKLEENS-MAYO-2024.pdf",
-    name: "Privacy",
+    href: '/pdf/AP-SDERKLEENS-MAYO-2024.pdf',
+    name: 'Privacy',
   },
   {
-    href: "/pdf/TYC-SDERKLEENS-MAYO-2024.pdf",
-    name: "Terms",
+    href: '/pdf/TYC-SDERKLEENS-MAYO-2024.pdf',
+    name: 'Terms',
   },
 ];
